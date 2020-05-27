c732e239aafe713fde5fac29bc0adbf7c270c434
Bailey Day <bailey.j.day@gmail.com>
2020-05-27 13:54:16 -0400

    Added middleware pathing and routes

--- practice-1.js
+++ practice-1.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("Im the middleware");
-  next();
+app.use("/add-product", (req, res, next) => {
+  console.log("Im another middleware");
+  res.send("<h1>The 'Add product' Page</h1>");
 });
 
-app.use((req, res, next) => {
+app.use("/", (req, res, next) => {
   console.log("Im another middleware");
   res.send("<h1>Hello from express!<h1>");
 });
