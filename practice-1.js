5ca38d42f7303ec0c33b803e4fa5485fa5a0c7b8
Bailey Day <bailey.j.day@gmail.com>
2020-06-02 15:01:48 -0400

    Added a form that sends a post request and body parser.

--- practice-1.js
+++ practice-1.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/", (req, res, next) => {
+  next();
+});
 
 app.use("/add-product", (req, res, next) => {
-  console.log("Im another middleware");
-  res.send("<h1>The 'Add product' Page</h1>");
+  res.send(
+    '<form action="/product" method="POST"><input type="Text" name="title"><button type="submit">Add Product</button></form>'
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-  console.log("Im another middleware");
   res.send("<h1>Hello from express!<h1>");
 });
 
