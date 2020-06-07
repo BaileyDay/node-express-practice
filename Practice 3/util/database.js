667292c1bd027a4ea74dd16ab83d47e9d04e101b
Bailey Day <bailey.j.day@gmail.com>
2020-06-07 15:04:12 -0400

    Set up connection to mongo database

--- Practice 3/util/database.js
+++ Practice 3/util/database.js
@@ -1,8 +1,17 @@
-const Sequelize = require('sequelize');
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const MongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://Bailey:password123!@cluster0-jzzpv.mongodb.net/<dbname>?retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      console.log("connected!");
+      callback(client);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
-module.exports = sequelize;
+module.exports = MongoConnect;
