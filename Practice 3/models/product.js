071551a90ae1afa07e81cf6f56053cf07ba902d8
Bailey Day <bailey.j.day@gmail.com>
2020-06-09 13:38:19 -0400

    Finished wrtiting connection for mongoDB

--- Practice 3/models/product.js
+++ Practice 3/models/product.js
@@ -1,4 +1,4 @@
-const mongoConnect = require("../util/database");
+const getDb = require("../util/database").getDB;
 
 class Product {
   constructor(title, price, description, imageURL) {
--- Practice 3/util/database.js
+++ Practice 3/util/database.js
@@ -1,17 +1,29 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const MongoConnect = (callback) => {
   MongoClient.connect(
-    "mongodb+srv://Bailey:password123!@cluster0-jzzpv.mongodb.net/<dbname>?retryWrites=true&w=majority"
+    "mongodb+srv://Bailey:password123!@cluster0-jzzpv.mongodb.net/shop?retryWrites=true&w=majority"
   )
     .then((client) => {
       console.log("connected!");
+      _db = client.db();
       callback(client);
     })
     .catch((err) => {
       console.log(err);
+      throw err;
     });
 };
 
-module.exports = MongoConnect;
+const getDB = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "no database found";
+};
+
+exports.MongoConnect = MongoConnect;
+exports.getDB = getDB;
