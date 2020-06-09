0c6a16afea2190d06ad5dccba269bda1f63a5c82
Bailey Day <bailey.j.day@gmail.com>
2020-06-09 13:51:48 -0400

    Began work on refactoring models and routes

--- Practice 3/models/product.js
+++ Practice 3/models/product.js
@@ -7,29 +7,14 @@ class Product {
     this.description = description;
     this.imageURL = imageURL;
   }
-  save() {}
+  save() {
+    const db = getDb;
+    db.collection("products")
+      .insertOne(this)
+      .then((result) => {
+        console.log(result);
+      });
+  }
 }
-
-const Product = sequelize.define("product", {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
-  },
-  title: Sequelize.STRING,
-  price: {
-    type: Sequelize.DOUBLE,
-    allowNull: false,
-  },
-  imageUrl: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  description: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-});
 
 module.exports = Product;
